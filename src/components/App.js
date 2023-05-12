6fdde831cc6bd6137b4793e259cf0929b5053de0
afrobug <afrojahmed6@gmail.com>
2023-05-13 01:34:30 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,22 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state = {
+			showPara: false
+		};
+		this.handleClick = this.handleClick.bind(this);
 	};
-
+	handleClick(){
+		this.setState({
+			showPara: true
+		});
+	}
     render() {
     	return(
     		<div id="main">
 				{ /* Do not remove this main div!! */ }
+							<button id="click" onClick={this.handleClick}>click me</button>
+				{this.state.showPara && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     		</div>
     	);
     }
